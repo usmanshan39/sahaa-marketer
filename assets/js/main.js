8720736dd842bcfd6bec0acf692dc6238bbc4d5e
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-08-04 17:35:21 +0500

    complete the header with navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,9 @@
-$(document).ready(function () {
-  // add navbar scroll effect
-  if ($(window).width() > 990) {
-    $(window).scroll(function () {
-      var scroll = $(window).scrollTop();
-      if (scroll > 250) {
-        $(".navbar").addClass('colour-navbar');
-      } else {
-        $(".navbar").removeClass('colour-navbar');
-      }
-    });
-  } else {
-    $(".navbar").addClass('colour-navbar');
-  }
-
-  // change text in main header
-  var words = [
-      'Reaults?',
-      'Sales?',
-      'Growth?',
-      'Connections?',
-    ],
-    i = 0;
-  setInterval(function () {
-    $('#changingword').fadeOut(function () {
-      $(this).html(words[i = (i + 1) % words.length]).fadeIn();
-    });
-  }, 2000);
-
-
-})
+$( document ).ready(function() {
+  // navbar toggle button
+  $("#navbar-toggler").on("click", function(e){
+    e.preventDefault();
+    $(this).children(".fa-bars").toggleClass("d-none");
+    $(this).children(".fa-times").toggleClass("d-none");
+  })
+  
+});
