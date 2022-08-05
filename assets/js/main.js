1360b2b4990c3aade8f306ea43c2bf8f4d84d2ec
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-08-05 17:20:44 +0500

    add greate partner section

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,27 @@ $( document ).ready(function() {
     e.preventDefault();
     $(this).children(".fa-bars").toggleClass("d-none");
     $(this).children(".fa-times").toggleClass("d-none");
+  });
+
+
+  // owl carosal
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    margin: 10,
+    loop: true,
+    nav: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
   })
   
 });
