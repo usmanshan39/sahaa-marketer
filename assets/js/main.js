67d9a608b0f571c35427872b9087717b6f78c1f8
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-08-12 19:09:33 +0500

    add number counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,5 +49,20 @@ $( document ).ready(function() {
       }
     }
   })
+
+  // meet the team page counter
+
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
   
 });
